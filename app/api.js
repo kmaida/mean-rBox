009149f1c38296fe8774c36bf8a5360775cea2e7
Kim Maida <git@kim-maida.com>
2015-05-19 11:19:47 -0400

    * Clean up extraneous code from User model / API.

--- app/api.js
+++ app/api.js
@@ -129,46 +129,6 @@ module.exports = function(app, config) {
 
 	/*
 	 |--------------------------------------------------------------------------
-	 | Log in with Email
-	 |--------------------------------------------------------------------------
-	 */
-	//app.post('/auth/login', function(req, res) {
-	//	User.findOne({ email: req.body.email }, '+password', function(err, user) {
-	//		if (!user) {
-	//			return res.status(401).send({ message: 'Wrong email and/or password' });
-	//		}
-	//		user.comparePassword(req.body.password, function(err, isMatch) {
-	//			if (!isMatch) {
-	//				return res.status(401).send({ message: 'Wrong email and/or password' });
-	//			}
-	//			res.send({ token: createToken(user) });
-	//		});
-	//	});
-	//});
-
-	/*
-	 |--------------------------------------------------------------------------
-	 | Create Email and Password Account
-	 |--------------------------------------------------------------------------
-	 */
-	//app.post('/auth/signup', function(req, res) {
-	//	User.findOne({ email: req.body.email }, function(err, existingUser) {
-	//		if (existingUser) {
-	//			return res.status(409).send({ message: 'Email is already taken' });
-	//		}
-	//		var user = new User({
-	//			displayName: req.body.displayName,
-	//			email: req.body.email,
-	//			password: req.body.password
-	//		});
-	//		user.save(function() {
-	//			res.send({ token: createToken(user) });
-	//		});
-	//	});
-	//});
-
-	/*
-	 |--------------------------------------------------------------------------
 	 | Login with Google
 	 |--------------------------------------------------------------------------
 	 */
--- app/models/User.js
+++ app/models/User.js
@@ -5,13 +5,10 @@
  */
 
 var mongoose = require('mongoose');
-//var bcrypt = require('bcryptjs');
 var encrypt = require('mongoose-encryption');
 var config = require('../config');
 
 var userSchema = new mongoose.Schema({
-	//email: { type: String, sparse: true, lowercase: true },
-	//password: { type: String, select: false },
 	isAdmin: Boolean,
 	displayName: String,
 	picture: String,
@@ -35,27 +32,6 @@ userSchema.plugin(encrypt, {
 	secret: config.TOKEN_SECRET,
 	encryptedFields: ['isAdmin']
 });
-
-/*
-userSchema.pre('save', function(next) {
-	var user = this;
-
-	if (!user.isModified('password')) {
-		return next();
-	}
-	bcrypt.genSalt(10, function(err, salt) {
-		bcrypt.hash(user.password, salt, function(err, hash) {
-			user.password = hash;
-			next();
-		});
-	});
-});
-
-userSchema.methods.comparePassword = function(password, done) {
-	bcrypt.compare(password, this.password, function(err, isMatch) {
-		done(err, isMatch);
-	});
-}; */
 
 var User = mongoose.model('User', userSchema);
 
