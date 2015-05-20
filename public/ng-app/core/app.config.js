cbe3325b511ec64e6b0a91a4302cc9dc6466bbda
Kim Maida <git@kim-maida.com>
2015-05-20 10:19:46 -0400

    * Tweak nav styles. * Move controllers into config.

--- public/ng-app/core/app.config.js
+++ public/ng-app/core/app.config.js
@@ -11,29 +11,43 @@
 	function appConfig($routeProvider, $locationProvider) {
 		$routeProvider
 			.when('/', {
-				templateUrl: 'ng-app/home/Home.view.html'
+				templateUrl: 'ng-app/home/Home.view.html',
+				controller: 'HomeCtrl',
+				controllerAs: 'home'
 			})
 			.when('/login', {
-				templateUrl: 'ng-app/login/Login.view.html'
+				templateUrl: 'ng-app/login/Login.view.html',
+				controller: 'LoginCtrl',
+				controllerAs: 'login'
+			})
+			.when('/recipe/:slug', {
+				templateUrl: 'ng-app/recipe/Recipe.view.html',
+				controller: 'RecipeCtrl',
+				controllerAs: 'recipe'
 			})
 			.when('/my-recipes', {
 				templateUrl: 'ng-app/my-recipes/MyRecipes.view.html',
-				secure: true
-			})
-			.when('/recipe/:slug', {
-				templateUrl: 'ng-app/recipe/Recipe.view.html'
+				secure: true,
+				controller: 'MyRecipesCtrl',
+				controllerAs: 'myRecipes'
 			})
 			.when('/recipe/:slug/edit', {
 				templateUrl: 'ng-app/recipe/EditRecipe.view.html',
-				secure: true
+				secure: true,
+				controller: 'EditRecipeCtrl',
+				controllerAs: 'edit'
 			})
 			.when('/account', {
 				templateUrl: 'ng-app/account/Account.view.html',
-				secure: true
+				secure: true,
+				controller: 'AccountCtrl',
+				controllerAs: 'account'
 			})
 			.when('/admin', {
 				templateUrl: 'ng-app/admin/Admin.view.html',
-				secure: true
+				secure: true,
+				controller: 'AdminCtrl',
+				controllerAs: 'admin'
 			})
 			.otherwise({
 				redirectTo: '/'
