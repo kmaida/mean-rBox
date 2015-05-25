fa90154fde7a8d602bc9c93b3e56b718be171fa0
Kim Maida <git@kim-maida.com>
2015-05-25 16:01:58 -0400

    FIX minToH filter to display proper label suffixes. * Add hours display to recipe form. * Style.

--- public/ng-app/recipe/minToH.filter.js
+++ public/ng-app/recipe/minToH.filter.js
@@ -8,18 +8,35 @@
 	function minToH() {
 		return function(min) {
 			var _hour = 60;
-			var _gtHour = min / _hour >= 1;
+			var _min = min * 1;
+			var _gtHour = _min / _hour >= 1;
 			var timeStr = null;
 
-			if (_gtHour) {
-				var hPlusMin = min % _hour;
-				var _hasMinutes = hPlusMin !== 0;
-				var hours = Math.floor(min / _hour);
-				var minutes = _hasMinutes ? ', ' + hPlusMin + ' minutes' : '';
+			/**
+			 * Get minute/s text from minutes
+			 *
+			 * @param minutes {number}
+			 * @returns {string}
+			 */
+			function minText(minutes) {
+				if (_hasMinutes && minutes === 1) {
+					return ' minute';
+				} else if (_hasMinutes && minutes !== 1) {
+					return ' minutes';
+				}
+			}
 
-				timeStr = hours + ' hours' + minutes;
+			if (_gtHour) {
+				var hPlusMin = _min % _hour;
+				var _hasMinutes = hPlusMin !== 0;
+				var hours = Math.floor(_min / _hour);
+				var hoursText = hours === 1 ? ' hour' : ' hours';
+				var minutes = _hasMinutes ? ', ' + hPlusMin + minText(hPlusMin) : '';
+
+				timeStr = hours + hoursText + minutes;
 			} else {
-				timeStr = min + ' minutes';
+				var noHMinText = _min === 1 ? ' minute' : ' minutes';
+				timeStr = _min + noHMinText;
 			}
 
 			return timeStr;
