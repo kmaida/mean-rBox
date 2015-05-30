194ff56d05ff8a6b02f963e4b5737d4425f34410
Kim Maida <git@kim-maida.com>
2015-05-30 15:28:12 -0400

    * Improve FIX #3 - repair Safari's false negative for CSS3 transforms3d.

--- public/assets/js/helpers.js
+++ public/assets/js/helpers.js
@@ -18,13 +18,16 @@ window.helpers = (function() {
 	function fixBrowsers() {
 		var ua = navigator.userAgent.toLowerCase(),
 			chrome = ua.lastIndexOf('chrome/') > 0,
+			safari = ua.lastIndexOf('safari') > 0,
 			$html = $('html');
 		
 		// Modernizr 2 bug: Chrome on Windows 8 gives a false negative for transforms3d support
 		// Google does not plan to fix this; https://code.google.com/p/chromium/issues/detail?id=129004
-		if (chrome) {
+		// Safari bug: Safari also gives false negative (seems site-specific)
+		if (chrome || safari) {
 			var chromeversion = ua.substr(ua.lastIndexOf('chrome/') + 7, 2);
-			if (chromeversion >= 12 && $html.hasClass('no-csstransforms3d')) {
+
+			if ((chromeversion >= 12 || safari) && $html.hasClass('no-csstransforms3d')) {
 				$html
 					.removeClass('no-csstransforms3d')
 					.addClass('csstransforms3d');
