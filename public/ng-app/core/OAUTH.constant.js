528e53ef9745f9ad9f2ad58a9494ba8ca85ec195
Kim Maida <git@kim-maida.com>
2015-11-29 22:52:57 -0500

    #41 - move OAUTH into app-setup

--- public/ng-app/core/OAUTH.constant.js
+++ public/ng-app/core/OAUTH.constant.js
@@ -2,27 +2,29 @@
 (function() {
 	'use strict';
 
+	var OAUTH = {
+		LOGINS: [
+			{
+				account: 'google',
+				name: 'Google',
+				url: 'http://accounts.google.com'
+			}, {
+				account: 'twitter',
+				name: 'Twitter',
+				url: 'http://twitter.com'
+			}, {
+				account: 'facebook',
+				name: 'Facebook',
+				url: 'http://facebook.com'
+			}, {
+				account: 'github',
+				name: 'GitHub',
+				url: 'http://github.com'
+			}
+		]
+	};
+
 	angular
 		.module('rBox')
-		.constant('OAUTH', {
-			LOGINS: [
-				{
-					account: 'google',
-					name: 'Google',
-					url: 'http://accounts.google.com'
-				}, {
-					account: 'twitter',
-					name: 'Twitter',
-					url: 'http://twitter.com'
-				}, {
-					account: 'facebook',
-					name: 'Facebook',
-					url: 'http://facebook.com'
-				}, {
-					account: 'github',
-					name: 'GitHub',
-					url: 'http://github.com'
-				}
-			]
-		});
+		.constant('OAUTH', OAUTH);
 })();
---
+++ public/ng-app/core/app-setup/OAUTHCLIENTS.SAMPLE.constant.js
@@ -0,0 +1,18 @@
+// login/Oauth constants
+(function() {
+	'use strict';
+
+	var OAUTHCLIENTS = {
+		LOGINURL: 'http://localhost:8080/auth/login',
+		CLIENT: {
+			FB: '[your Facebook client ID]',
+			GOOGLE: '[your Google client ID]',
+			TWITTER: '/auth/twitter',
+			GITHUB: '[your GitHub client ID]'
+		}
+	};
+
+	angular
+		.module('rBox')
+		.constant('OAUTHCLIENTS', OAUTHCLIENTS);
+})();
