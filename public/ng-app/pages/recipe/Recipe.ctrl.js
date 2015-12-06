94c4d32f0c1707e2710d01f75122c5498a272453
Kim Maida <git@kim-maida.com>
2015-12-05 22:32:45 -0500

    #42 - optimize RecipeCtrl

--- public/ng-app/pages/recipe/Recipe.ctrl.js
+++ public/ng-app/pages/recipe/Recipe.ctrl.js
@@ -5,14 +5,48 @@
 		.module('rBox')
 		.controller('RecipeCtrl', RecipeCtrl);
 
-	RecipeCtrl.$inject = ['Page', 'Utils', '$routeParams', 'recipeData', 'userData'];
+	RecipeCtrl.$inject = ['$scope', 'Page', 'Utils', '$routeParams', 'recipeData', 'userData'];
 
-	function RecipeCtrl(Page, Utils, $routeParams, recipeData, userData) {
+	function RecipeCtrl($scope, Page, Utils, $routeParams, recipeData, userData) {
 		// controllerAs ViewModel
 		var recipe = this;
+
+		// private variables
 		var recipeSlug = $routeParams.slug;
 
-		Page.setTitle('Recipe');
+		// bindable members
+		recipe.ingChecked = [];
+		recipe.stepChecked = [];
+		recipe.toggleCheck = toggleCheck;
+		recipe.fileRecipe = fileRecipe;
+
+		_init();
+
+		/**
+		 * INIT
+		 *
+		 * @private
+		 */
+		function _init() {
+			Page.setTitle('Recipe');
+
+			_activate();
+		}
+
+		/**
+		 * ACTIVATE
+		 *
+		 * @private
+		 */
+		function _activate() {
+			$scope.$emit('loading-on');
+
+			if (Utils.isAuthenticated()) {
+				userData.getUser().then(_getUserSuccess);
+			}
+
+			recipeData.getRecipe(recipeSlug).then(_recipeSuccess, _recipeError);
+		}
 
 		/**
 		 * Successful promise returning user's data
@@ -27,10 +61,6 @@
 			recipe.fileText = 'File this recipe';
 			recipe.unfileText = 'Remove from Filed Recipes';
 		}
-		if (Utils.isAuthenticated()) {
-			userData.getUser()
-				.then(_getUserSuccess);
-		}
 
 		/**
 		 * Successful promise returning recipe data
@@ -39,52 +69,32 @@
 		 * @private
 		 */
 		function _recipeSuccess(data) {
-			var i;
-
 			recipe.recipe = data;
 			Page.setTitle(recipe.recipe.name);
 
-			/**
-			 * Successful promise returning author data
-			 *
-			 * @param data {promise} author picture, displayName
-			 * @private
-			 */
-			function _authorSuccess(data) {
-				recipe.author = data;
-			}
-			userData.getAuthor(recipe.recipe.userId)
-				.then(_authorSuccess);
-
-			recipe.ingChecked = [];
-			recipe.stepChecked = [];
-
-			/**
-			 * Create array to keep track of checked / unchecked items
-			 *
-			 * @param checkedArr
-			 * @param sourceArr
-			 * @private
-			 */
-			function _createCheckedArrays(checkedArr, sourceArr) {
-				for (i = 0; i < sourceArr.length; i++) {
-					checkedArr[i] = false;
-				}
-			}
+			userData.getAuthor(recipe.recipe.userId).then(_authorSuccess);
 
 			_createCheckedArrays(recipe.ingChecked, recipe.recipe.ingredients);
 			_createCheckedArrays(recipe.stepChecked, recipe.recipe.directions);
 
-			/**
-			 * Toggle checkmark
-			 *
-			 * @param type
-			 * @param index
-			 */
-			recipe.toggleCheck = function(type, index) {
-				recipe[type + 'Checked'][index] = !recipe[type + 'Checked'][index];
-			};
+			$scope.$emit('loading-off');
 		}
+
+		/**
+		 * Create array to keep track of checked / unchecked items
+		 *
+		 * @param checkedArr
+		 * @param sourceArr
+		 * @private
+		 */
+		function _createCheckedArrays(checkedArr, sourceArr) {
+			var i;
+
+			for (i = 0; i < sourceArr.length; i++) {
+				checkedArr[i] = false;
+			}
+		}
+
 		/**
 		 * Error retrieving recipe
 		 *
@@ -95,39 +105,60 @@
 			recipe.recipe = 'error';
 			Page.setTitle('Error');
 			recipe.errorMsg = res.data.message;
+
+			$scope.$emit('loading-off');
 		}
-		recipeData.getRecipe(recipeSlug)
-			.then(_recipeSuccess, _recipeError);
+
+		/**
+		 * Toggle checkmark
+		 *
+		 * @param type
+		 * @param index
+		 */
+		function toggleCheck(type, index) {
+			recipe[type + 'Checked'][index] = !recipe[type + 'Checked'][index];
+		}
+
+		/**
+		 * Successful promise returning author data
+		 *
+		 * @param data {promise} author picture, displayName
+		 * @private
+		 */
+		function _authorSuccess(data) {
+			recipe.author = data;
+		}
 
 		/**
 		 * File or unfile this recipe
 		 *
 		 * @param recipeId {string} ID of recipe to save
+		 * @returns {promise}
 		 */
-		recipe.fileRecipe = function(recipeId) {
-			/**
-			 * Successful promise from saving recipe to user data
-			 *
-			 * @param data {promise}.data
-			 * @private
-			 */
-			function _fileSuccess(data) {
-				console.log(data.message);
-				recipe.apiMsg = data.added ? 'Recipe saved!' : 'Recipe removed!';
-				recipe.filed = data.added;
-			}
+		function fileRecipe(recipeId) {
+			return recipeData.fileRecipe(recipeId).then(_fileSuccess, _fileError);
+		}
 
-			/**
-			 * Error promise from saving recipe to user data
-			 *
-			 * @param response {promise}
-			 * @private
-			 */
-			function _fileError(response) {
-				console.log(response.data.message);
-			}
-			recipeData.fileRecipe(recipeId)
-				.then(_fileSuccess, _fileError);
-		};
+		/**
+		 * Successful promise from saving recipe to user data
+		 *
+		 * @param data {promise}.data
+		 * @private
+		 */
+		function _fileSuccess(data) {
+			console.log(data.message);
+			recipe.apiMsg = data.added ? 'Recipe saved!' : 'Recipe removed!';
+			recipe.filed = data.added;
+		}
+
+		/**
+		 * Error promise from saving recipe to user data
+		 *
+		 * @param response {promise}
+		 * @private
+		 */
+		function _fileError(response) {
+			console.log(response.data.message);
+		}
 	}
 }());
