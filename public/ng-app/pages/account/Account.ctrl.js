57789418030460ee0dfa8ec0462b714c28d480d9
Kim Maida <git@kim-maida.com>
2015-12-05 21:32:39 -0500

    #41 - optimize AccountCtrl

--- public/ng-app/pages/account/Account.ctrl.js
+++ public/ng-app/pages/account/Account.ctrl.js
@@ -10,10 +10,11 @@
 	function AccountCtrl($scope, Page, Utils, $auth, userData, $timeout, OAUTH, User, $location) {
 		// controllerAs ViewModel
 		var account = this;
+
+		// private variables
 		var _tab = $location.search().view;
 
-		Page.setTitle('My Account');
-
+		// bindable members
 		account.tabs = [
 			{
 				name: 'User Info',
@@ -25,53 +26,69 @@
 			}
 		];
 		account.currentTab = _tab ? _tab : 'user-info';
+		account.logins = OAUTH.LOGINS;  // all available login services
+		account.isAuthenticated = Utils.isAuthenticated;
+		account.changeTab = changeTab;
+		account.getProfile = getProfile;
+		account.updateProfile = updateProfile;
+		account.link = link;
+		account.unlink = unlink;
+
+		_init();
+
+		/**
+		 * INIT
+		 *
+		 * @private
+		 */
+		function _init() {
+			Page.setTitle('My Account');
+			_btnSaveReset();
+			$scope.$watch('account.user.displayName', _$watchDisplayName);
+			account.getProfile();
+		}
 
 		/**
 		 * Change tab
 		 *
 		 * @param query {string} tab to switch to
 		 */
-		account.changeTab = function(query) {
+		function changeTab(query) {
 			$location.search('view', query);
 			account.currentTab = query;
-		};
-
-		// all available login services
-		account.logins = OAUTH.LOGINS;
-
-		account.isAuthenticated = Utils.isAuthenticated;
+		}
 
 		/**
 		 * Get user's profile information
 		 */
-		account.getProfile = function() {
-			/**
-			 * Function for successful API call getting user's profile data
-			 * Show Account UI
-			 *
-			 * @param data {object} promise provided by $http success
-			 * @private
-			 */
-			function _getUserSuccess(data) {
-				account.user = data;
-				account.administrator = account.user.isAdmin;
-				account.linkedAccounts = User.getLinkedAccounts(account.user, 'account');
-				account.showAccount = true;
-			}
+		function getProfile() {
+			return userData.getUser().then(_getUserSuccess, _getUserError);
+		}
 
-			/**
-			 * Function for error API call getting user's profile data
-			 * Show an error alert in the UI
-			 *
-			 * @param error
-			 * @private
-			 */
-			function _getUserError(error) {
-				account.errorGettingUser = true;
-			}
+		/**
+		 * Function for successful API call getting user's profile data
+		 * Show Account UI
+		 *
+		 * @param data {object} promise provided by $http success
+		 * @private
+		 */
+		function _getUserSuccess(data) {
+			account.user = data;
+			account.administrator = account.user.isAdmin;
+			account.linkedAccounts = User.getLinkedAccounts(account.user, 'account');
+			account.showAccount = true;
+		}
 
-			userData.getUser().then(_getUserSuccess, _getUserError);
-		};
+		/**
+		 * Function for error API call getting user's profile data
+		 * Show an error alert in the UI
+		 *
+		 * @param error
+		 * @private
+		 */
+		function _getUserError(error) {
+			account.errorGettingUser = true;
+		}
 
 		/**
 		 * Reset profile save button to initial state
@@ -83,8 +100,6 @@
 			account.btnSaveText = 'Save';
 		}
 
-		_btnSaveReset();
-
 		/**
 		 * Watch display name changes to check for empty or null string
 		 * Set button text accordingly
@@ -93,43 +108,20 @@
 		 * @param oldVal {*} previous displayName value
 		 * @private
 		 */
-		function _watchDisplayName(newVal, oldVal) {
+		function _$watchDisplayName(newVal, oldVal) {
 			if (newVal === '' || newVal === null) {
 				account.btnSaveText = 'Enter Name';
 			} else {
 				account.btnSaveText = 'Save';
 			}
 		}
-		$scope.$watch('account.user.displayName', _watchDisplayName);
 
 		/**
 		 * Update user's profile information
 		 * Called on submission of update form
 		 */
-		account.updateProfile = function() {
+		function updateProfile() {
 			var profileData = { displayName: account.user.displayName };
-
-			/**
-			 * Success callback when profile has been updated
-			 *
-			 * @private
-			 */
-			function _updateSuccess() {
-				account.btnSaved = true;
-				account.btnSaveText = 'Saved!';
-
-				$timeout(_btnSaveReset, 2500);
-			}
-
-			/**
-			 * Error callback when profile update has failed
-			 *
-			 * @private
-			 */
-			function _updateError() {
-				account.btnSaved = 'error';
-				account.btnSaveText = 'Error saving!';
-			}
 
 			if (account.user.displayName) {
 				// Set status to Saving... and update upon success or error in callbacks
@@ -138,38 +130,54 @@
 				// Update the user, passing profile data and assigning success and error callbacks
 				userData.updateUser(profileData).then(_updateSuccess, _updateError);
 			}
-		};
+		}
+
+		/**
+		 * Success callback when profile has been updated
+		 *
+		 * @private
+		 */
+		function _updateSuccess() {
+			account.btnSaved = true;
+			account.btnSaveText = 'Saved!';
+
+			$timeout(_btnSaveReset, 2500);
+		}
+
+		/**
+		 * Error callback when profile update has failed
+		 *
+		 * @private
+		 */
+		function _updateError() {
+			account.btnSaved = 'error';
+			account.btnSaveText = 'Error saving!';
+		}
 
 		/**
 		 * Link third-party provider
 		 *
 		 * @param {string} provider
 		 */
-		account.link = function(provider) {
+		function link(provider) {
 			$auth.link(provider)
-				.then(function() {
-					account.getProfile();
-				})
+				.then(account.getProfile)
 				.catch(function(response) {
 					alert(response.data.message);
 				});
-		};
+		}
 
 		/**
 		 * Unlink third-party provider
 		 *
 		 * @param {string} provider
 		 */
-		account.unlink = function(provider) {
+		function unlink(provider) {
 			$auth.unlink(provider)
-				.then(function() {
-					account.getProfile();
-				})
+				.then(account.getProfile)
 				.catch(function(response) {
 					alert(response.data ? response.data.message : 'Could not unlink ' + provider + ' account');
 				});
-		};
-
-		account.getProfile();
+		}
 	}
 }());
