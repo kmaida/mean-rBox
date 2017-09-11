f0a0a3d9a6e7feae83c658b3462a8e4869bde010
Kim Maida <kim.maida@auth0.com>
2017-09-11 09:41:13 -0400

    Update server for Mongo useMongoClient

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ var config = require('./app/config');
  |--------------------------------------------------------------------------
  */
 
-mongoose.openUri(config.MONGO_URI);
+mongoose.connect(config.MONGO_URI, { useMongoClient: true });
 var monDb = mongoose.connection;
 
 monDb.on('error', function() {
